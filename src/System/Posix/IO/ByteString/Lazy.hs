edd9f69a5ae3f1954db90fc4413f0b764ff1de8b
wren ng thornton <wren@community.haskell.org>
2011-03-05 09:06:51 +0000

    System.Posix.IO.ByteString.Lazy: changed fdWrite to fdWrites

--- src/System/Posix/IO/ByteString/Lazy.hs
+++ src/System/Posix/IO/ByteString/Lazy.hs
@@ -1,13 +1,14 @@
 {-# OPTIONS_GHC -Wall -fwarn-tabs #-}
+{-# LANGUAGE BangPatterns #-}
 ----------------------------------------------------------------
---                                                    2011.02.27
+--                                                    2011.03.04
 -- |
 -- Module      :  System.Posix.IO.ByteString.Lazy
 -- Copyright   :  Copyright (c) 2010--2011 wren ng thornton
 -- License     :  BSD
 -- Maintainer  :  wren@community.haskell.org
 -- Stability   :  provisional
--- Portability :  non-portable (Posix)
+-- Portability :  non-portable (requires POSIX)
 --
 -- Provides lazy 'ByteString' versions of the "System.Posix.IO"
 -- file-descriptor based I\/O API.
@@ -16,17 +17,18 @@ module System.Posix.IO.ByteString.Lazy
     (
     -- * I\/O with file descriptors
       fdRead
-    , fdWrite
+    , fdWrites
+    -- TODO: fdWritev
     ) where
 
 import qualified Data.ByteString               as BS
+import qualified Data.ByteString.Unsafe        as BSU
 import qualified System.Posix.IO.ByteString    as PosixBS
 import qualified Data.ByteString.Lazy          as BL
 import qualified Data.ByteString.Lazy.Internal as BLI
 import           System.Posix.Types            (Fd, ByteCount)
 
 ----------------------------------------------------------------
-
 -- | Read data from an 'Fd' and convert it to a 'BL.ByteString'.
 -- Throws an exception if this is an invalid descriptor, or EOF has
 -- been reached.
@@ -38,23 +40,52 @@ fdRead
 fdRead _  0 = return (BL.empty, 0)
 fdRead fd n = do
     (s,n') <- PosixBS.fdRead fd n
-    return (BL.fromChunks [s], n')
+    return (BLI.chunk s BL.empty, n')
 
 
--- | Write a 'BL.ByteString' to an 'Fd'.
-fdWrite :: Fd -> BL.ByteString -> IO ByteCount
-fdWrite fd = go 0
+----------------------------------------------------------------
+-- | Write a 'BL.ByteString' to an 'Fd'. The return value is a pair
+-- of the total number of bytes written, and the remaining (unwritten)
+-- input. This function makes one @write@ system call per chunk,
+-- as per 'PosixBS.fdWrites'.
+fdWrites :: Fd -> BL.ByteString -> IO (ByteCount, BL.ByteString)
+fdWrites fd = go 0
     where
     -- We want to do a left fold in order to avoid stack overflows,
     -- but we need to have an early exit for incomplete writes
     -- (which normally requires a right fold). Hence this recursion.
-    go acc BLI.Empty        = return acc
+    go acc BLI.Empty        = return (acc, BL.empty)
     go acc (BLI.Chunk c cs) = do
         rc <- PosixBS.fdWrite fd c
-        let acc' = acc+rc in acc' `seq` do
-        if rc == fromIntegral (BS.length c)
+        let !acc'  = acc+rc
+            !rcInt = fromIntegral rc
+        if rcInt == BS.length c
             then go acc' cs
-            else return acc'
+            else return (acc', BLI.Chunk (BSU.unsafeDrop rcInt c) cs)
+{-
+Using 'BSU.unsafeDrop' above is safe, assuming that
+'System.Posix.IO.fdWriteBuf' never returns (rc < 0 || rc > BS.length c).
+If we are paranoid about that then we should do the following instead:
+
+    go acc ccs =
+        case ccs of
+        BLI.Empty      -> return (acc, ccs)
+        BLI.Chunk c cs -> do
+            rc <- PosixBS.fdWrite fd c
+            let !acc'  = acc+rc
+                !rcInt = fromIntegral rc
+            case BS.length c of
+                len | rcInt == len -> go acc' cs
+                    | rcInt >  len -> error _impossibleByteCount
+                    | rcInt <  0   -> error _negtiveByteCount
+                    | rcInt == 0   -> return (acc', ccs) -- trivial optimizing
+                    | otherwise    -> return (acc', BLI.Chunk (BSU.unsafeDrop rcInt c) cs)
+
+_negtiveByteCount =
+    "System.Posix.IO.fdWriteBuf: returned a negative byte count"
+_impossibleByteCount =
+    "System.Posix.IO.fdWriteBuf: returned a byte count greater than the length it was given"
+-}
 
 ----------------------------------------------------------------
 ----------------------------------------------------------- fin.
